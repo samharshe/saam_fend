3e5beb1934ab7fa3769e5e2eea0e05d0da248b5e
samharshe <samharshe29@gmail.com>
2025-05-14 18:18:53 -0700

    added favicon; modified wording of JS; fixed undesirable wrapping as screen shrinks.

--- demo/app.js
+++ demo/app.js
@@ -2,6 +2,6 @@
 document.querySelectorAll('.gallery img').forEach(img => {
     img.addEventListener('click', function() {
         let madeUpProb = Math.floor(Math.random() * 50) + 51;
-        document.querySelector('.log').innerHTML = `GPT-8o predicts that this is the logo of <b>${this.alt}</b> with probability ${madeUpProb}%.`;
+        document.querySelector('.log').innerHTML = `GPT-8o predicts that this is the logo of <b>${this.alt}</b> with confidence ${madeUpProb}%.`;
     });
 });
